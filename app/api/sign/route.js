e273c7e6d2328425a790ab3c6bceccd66b1728b7
Matthew Bryan <matthew@revvit.io>
2025-11-25 16:18:06 -0500

    fix: use pdfkit standalone to avoid font path error

--- app/api/sign/route.js
+++ app/api/sign/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
-import PDFDocument from 'pdfkit';
+import PDFDocument from 'pdfkit/js/pdfkit.standalone.js';
 
 const resend = new Resend(process.env.RESEND_API_KEY || '');
 
